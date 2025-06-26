e7af7be659b0f48875a2c3dc5a194e28d2e5dbb8
Rose <120544840+Rose-my@users.noreply.github.com>
2025-06-26 15:55:48 +0900

    Title: 피보나치 수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250626/피보나치 수/fibonacci-number.java
+++ 250626/피보나치 수/fibonacci-number.java
@@ -4,9 +4,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
+        
+        if (n == 1 || n == 2) {
+            System.out.println(1);
+            return;
+        }
+
         int[] f = new int[n+1];
         f[1]=1;
         f[2]=1;
+        
         for(int i=3; i<n+1; i++){
             f[i]= f[i-1]+f[i-2];
         }
@@ -14,4 +21,5 @@ public class Main {
         System.out.println(f[n]);
         
     }
+
 }
