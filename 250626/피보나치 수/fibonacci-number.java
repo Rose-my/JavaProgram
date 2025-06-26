10a2503eb3231a479bee6473d98b678543479308
Rose <120544840+Rose-my@users.noreply.github.com>
2025-06-26 15:52:16 +0900

    Title: 피보나치 수, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250626/피보나치 수/fibonacci-number.java
+++ 250626/피보나치 수/fibonacci-number.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
-        int[] f = new int[n+1];
+        int[] f = new int[46];
         f[1]=1;
         f[2]=1;
         for(int i=3; i<n+1; i++){
